e30bd26fbd1acbe03daade8e248c3bbcf199cafa
Gustav91 <hapaitata@hotmail.com>
2015-03-30 17:25:31 +0200

    started with the client

--- clientserver/client.cc
+++ clientserver/client.cc
@@ -5,6 +5,7 @@
 
 #include "connection.h"
 #include "connectionclosedexception.h"
+#include "protocol.h"
 
 using namespace std;
 
@@ -54,14 +55,88 @@ int main(int argc, char* argv[]) {
    * 3. Delete newsgroup 4. List articles in current newsgroup
    * 5. Create article 6. Delete article 7. Exit
    */
+  MessageHandler mh(conn);
+  Protocol protocol;
+  vector<pair<string, int>> newsGroups;
   cout << "Welcome!" << endl;
   while(true) {  
-    int com = userInput(cout, cin);
-    switch(com) {
-    case 1: 
-      //listNewsgroups(cout);
+    string com;
+	cin >> com;
+    if(com == "listNG") {
+		mh.sendCode(protocol.COM_LIST_NG);
+		mh.sendCode(protocol.COM_END);
+		if(mh.readCode() != protocol.ANS_LIST_NG){
+			cerr << "Wrong answer from server" << endl;
+			exit(1);
+		}
+		try{
+			newsGroups.clear();
+			int n = mh.readIntPar();
+			for(int i = 1; i != n + 1; ++i) {
+				int id = mh.readIntPar();
+				string title = mh.readStringPar();
+				newsGroups.push_back(make_pair(title, id));
+				cout << i << ". " << title << endl;
+			}
+			if(mh.readCode() != protocol.ANS_END){
+				cerr << "Wrong answer from server" << endl;
+				exit(1);
+			}
+		}catch(ConnectionClosedException e){
+			exit(1);
+		}
+		
+	}else if(com == "listART"){
+
+	}else if(com == "createNG"){
+		string name;
+		cin >> name;
+		if(name.size() == 0) {
+			cerr << "Wrong input format." << endl;
+		} else {
+		 mh.sendCode(protocol.COM_CREATE_NG);
+		mh.sendStringPar(name);
+		mh.sendCode(protocol.COM_END);
+		
+		if(mh.readCode() == protocol.ANS_CREATE_NG){
+			int answer = mh.readCode();
+			if(answer == protocol.ANS_ACK){
+				cout << name << " was added." << endl;
+			}else if(answer == protocol.ANS_NAK){
+				int errorCode = mh.readCode();
+				cout << name << " already exist. " << endl;
+			}else{
+				cerr << "Wrong answer from server" << endl;
+				exit(1);
+			}
+		}else{
+			cerr << "Wrong answer from server" << endl;
+			exit(1);
+		}
+		if(mh.readCode() != protocol.ANS_END){
+			cerr << "Wrong answer from server" << endl;
+			exit(1);
+		}
+	}else if(com == "deleteNG"){
+
+	}else if(com == "createART"){
+
+	}else if(com == "deleteART"){
+
+	}else if(com == "readART"){
+
+	}else if(com == "exit"){
+
+	}else{
+		cerr << "Undefined commando..." << endl;
+	}
+
+    case 1:
+		mh.sendCode(protocol.COM_LIST_NG);
+		mh.sendCode(protocol.COM_END);
       break;
     case 2:
+		
       //createNewsgroup();
       break;
     case 3:
@@ -98,9 +173,9 @@ int userInput(ostream& out, istream& in) {
   out << "1. List newsgroups\n2. Create newsgroup\n3. Delete newsgroup\n"
       << "4. List articles\n5. Create article\n6. Delete article\n7. Exit\n"
       << endl;
-  int input;
-  if((in >> input) < 0 || input > 8)
-    input = -1;
+  string com1;
+	
+  
   return input;
   
 }
